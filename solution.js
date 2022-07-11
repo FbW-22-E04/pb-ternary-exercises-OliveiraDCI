7dc6f56aa8426c1c3fadd8dc1246d9b84276da75
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-07-11 13:43:35 +0200

    Exercise concluded.

---
+++ solution.js
@@ -0,0 +1,34 @@
+// 1
+let isDog = true ? "pat, pat" : "find me a dog to pat!";
+console.log(isDog);
+
+// 2
+const speedLimit = 50;
+const yourSpeed = 60;
+console.log(
+  yourSpeed >= speedLimit
+    ? "you're going too fast!"
+    : "You're driving below the speed limit, Oma"
+);
+
+// 3
+const age = 18;
+console.log(age < 16 ? "serve butter beer" : "serve beer");
+
+// 4
+const isStudent = false;
+console.log(isStudent == true ? "Ticket costs €5,00" : "Ticket costs €12,00");
+
+// 5
+const okMarie = "cake";
+console.log(okMarie == "cake" ? "Let them eat cake" : "Oh, bother");
+
+// 6
+num1 = 30;
+console.log(num1 % 2 == 0 ? num1 + " is even" : num1 + " is odd");
+
+num2 = 939;
+console.log(num2 % 2 == 0 ? num2 + " is even" : num2 + " is odd");
+
+num3 = 40.9;
+console.log(num3 % 2 == 0 ? num3 + " is even" : num3 + " is odd");
